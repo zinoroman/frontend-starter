e83c81b10491353f6668531d2d300499094e25d7
Zino <zino.roman95@gmail.com>
2017-03-09 11:22:59 +0200

    Remove tabs, use spaces

--- gulp/tasks/livereload.js
+++ gulp/tasks/livereload.js
@@ -3,10 +3,10 @@ import browserSync from 'browser-sync';
 import paths from '../paths';
 
 gulp.task('browser-sync', () => {
-	return browserSync({
-		files: [
+    return browserSync({
+        files: [
             `${paths.app}/css/**/*.css`
-		],
-		server: paths.app
-	});
+        ],
+        server: paths.app
+    });
 });
